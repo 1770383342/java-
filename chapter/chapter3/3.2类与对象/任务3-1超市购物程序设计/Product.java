1f099723d7e1f22948e58c482bd0181a57afc66e
1770383342 <1770383342@qq.com>
2020-06-14 10:20:45 +0800

    任务3-1

---
+++ chapter/chapter3/3.2类与对象/任务3-1超市购物程序设计/Product.java
@@ -0,0 +1,14 @@
+
+public class Product {
+
+    private String proName;
+
+    public String getProName() {
+        return proName;
+    }
+
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+    
+}
