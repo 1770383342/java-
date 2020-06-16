887fd86af218b8dda31fd267a96ffdb0372b31b9
1770383342 <1770383342@qq.com>
2020-06-16 16:01:23 +0800

    修改

--- chapter/chapter3/任务3-2银行新用户现金业务办理/task02/task02/Bank.java
+++ chapter/chapter3/任务3-2银行新用户现金业务办理/task02/task02/Bank.java
@@ -6,11 +6,19 @@ public class Bank {
     private String password;
     private double balance;
     private double turnover;
-    //静态方法打印出银行欢迎语句
+
+    /**
+     * 静态方法打印出银行欢迎语句
+     */
     static void welcome() {
         System.out.println("欢迎来到" + bankName + "-----------------");
     }
-    //构造方法  开户
+    /**
+     * 构造方法  开户
+     * @param name
+     * @param passworld
+     * @param turnover
+     */
     public Bank(String name, String passworld, double turnover) {
         //将变量赋值给成员变量
         this.name = name;
@@ -19,12 +27,19 @@ public class Bank {
         this.balance = turnover - 10;
         System.out.println(name + "开户成功，账户余额" + balance);
     }
-    //存款
+    /**
+     * 存款
+     * @param turnover
+     */
     public void deposit(double turnover) {
         balance = balance + turnover;
         System.out.println(name + "您好，您的账户余 已存入" + turnover + "元，" + "当前余额" + balance + "元");
     }
-    //取款
+    /**
+     * 取款
+     * @param password
+     * @param turnover
+     */
     public void withdrawal(String password, double turnover) {
         //根据传入的变量对比，盘对密码是否正确
         if (this.password != password) {
@@ -38,7 +53,9 @@ public class Bank {
             System.out.println("对不起，账户余额不足");
         }
     }
-    //静态方法，打印出银行欢迎下次光临语句
+    /**
+     * 静态方法，打印出银行欢迎下次光临语句
+     */
     static void welcomeNext() {
         System.out.println("请携带好随身财物，欢迎下次光临" + bankName + "----------------");
     }
