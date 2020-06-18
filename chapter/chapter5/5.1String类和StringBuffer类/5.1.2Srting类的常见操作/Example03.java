121a600366b049de58d40252adab62e66316f285
1770383342 <1770383342@qq.com>
2020-06-18 16:38:49 +0800

    5.1

---
+++ chapter/chapter5/5.1String类和StringBuffer类/5.1.2Srting类的常见操作/Example03.java
@@ -0,0 +1,23 @@
+
+public class Example03 {
+
+    public static void main(String[] args) {
+
+        String str = "abcd";
+        System.out.println("将字符串转化为字符数组后的结果：");
+        // 字符串转化为字符数组
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (i != charArray.length - 1) {
+                // 如果不算数组的最后一个约束，在元素前面加逗号
+                System.out.println(charArray[i] + ",");
+            } else {
+                // 数组的最后以后元素后面不加逗号
+                System.out.println(charArray[i]);
+            }
+        }
+        System.out.println("将int值转换为String类型之后的结果：" + String.valueOf(12));
+        System.out.println("将字符串转换为大写之后的结果：" + str.toUpperCase() );
+    }
+
+}
