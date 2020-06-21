953c79c06a1e7a3924f48076f2ce182bddb00be2
1770383342 <1770383342@qq.com>
2020-06-21 17:36:18 +0800

    5.3.1

--- chapter/chapter5/5.3Math类和Random类/5.3.1Mathlei/Example18.java
+++ chapter/chapter5/5.3Math类和Random类/5.3.1Mathlei/Example18.java
@@ -3,10 +3,10 @@ import java.util.Random;
 public class Example18 {
 
     public static void main(String[] args) {
-        
+
         Random r1 = new Random();
-        System.out.println("产生float类型的随机数："+r1.nextFloat());
-        System.out.println("产生0~100之间的int类型随机数："+r1.nextInt(100));
-        System.out.println("产生double类型的随机数："+r1.nextDouble());
+        System.out.println("产生float类型的随机数：" + r1.nextFloat());
+        System.out.println("产生0~100之间的int类型随机数：" + r1.nextInt(100));
+        System.out.println("产生double类型的随机数：" + r1.nextDouble());
     }
 }
